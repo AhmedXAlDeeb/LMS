f9ae8124219cc378d77ac97dc9c3a4fa6ca5e4ed
Ahmed Al-Deeb <124098788+AhmedXAlDeeb@users.noreply.github.com>
2024-02-15 08:03:13 +0200

    أرجوك متعملش conflict

--- obj/Debug/net7.0-windows/SchoolManagementSystem.AssemblyInfo.cs
+++ obj/Debug/net7.0-windows/SchoolManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchoolManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0b98554d21c6216af78e3c78239ac2353e7768e")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchoolManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchoolManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
